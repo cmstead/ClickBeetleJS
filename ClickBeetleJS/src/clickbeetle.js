454457e6e5eafee4253119a269a3a4f9e1b6ef75
Chris Stead <cmstead@gmail.com>
2013-08-15 23:59:43 -0700

    Added menu bindings for focus. All tests pass.

--- ClickBeetleJS/src/clickbeetle.js
+++ ClickBeetleJS/src/clickbeetle.js
@@ -1,13 +1,45 @@
 (function(document, $){
 
-    var clickbeetle = new function(document, $){};
+    var ClickBeetle = function(document, $){
+        this.init();
+    };
 
-    clickbeetle.prototype = {
+    ClickBeetle.prototype = {
+        $menus: [],
+
         init: function(){
-            this.$menus = $(".clickbeetle-menu");
-        }
+            var index = -1;
+            this.$menus = $(document).find(".clickbeetle-menu");
+
+            while(typeof this.$menus[++index] !== "undefined"){
+                this.bindings(this.$menus[index]);
+            }
+        },
+
+        bindings: function($menu){
+            var anchors = $($menu).find('a'),
+                children = $($menu).children();
+
+            $($menu).find('a').bind("focus", this.focusAction);
+        },
+
+        focusAction: function(event){
+            var $target = event.target;
+
+            while(!$($target).hasClass("clickbeetle-menu")){
+                $target = $($target).parent();
+            }
+
+            if(!$($target).hasClass("show")){
+                $($target).addClass("show");
+            }
+        },
+
+        blurAction: function(){}
     }
 
-    ClickBeetle = new clickbeetle(document, $);
+    $(document).ready(function(document, $){
+        clickbeetle = new ClickBeetle(document, $);
+    });
 
 })(document, $);
--- ClickBeetleJS/tests/js/clickbeetle.main.tests.js
+++ ClickBeetleJS/tests/js/clickbeetle.main.tests.js
@@ -1,5 +1,29 @@
-module("ClickBeetle");
+module("ClickBeetle", {
+        setup: function(){},
+        teardown: function(){
+            while($(".clickbeetle-menu").hasClass("show")){
+                $(".clickbeetle-menu").removeClass("show");
+            }
+        }
+    });
 
-test("should be available for use.", function(){
-    notEqual(ClickBeetle, null, "ok");
+test("should be available for use", function(){
+    notEqual(typeof clickbeetle, "undefined");
 });
+
+test("should have a menu array conatining one element", function(){
+    equal(clickbeetle.$menus.length, 1);
+});
+
+test("should set the menu class to show when the main link gets focus", function(){
+    $($(".clickbeetle-menu").find('a')[0]).focus();
+    equal($($(".clickbeetle-menu")[0]).hasClass("show"), true);
+    $($(".clickbeetle-menu").find('a')[0]).focus();
+});
+
+test("should set the menu class to show when the last link gets focus", function(){
+    var anchors = $(".clickbeetle-menu").find('a');
+    $(anchors[anchors.length - 1]).focus();
+    equal($($(".clickbeetle-menu")[0]).hasClass("show"), true);
+});
+
