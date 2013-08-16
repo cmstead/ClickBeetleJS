7a6efc13690cb724e48ad5d9b208a55ae19a625a
Christian Stead <cstead@mac-lap-cstead.bridgepoint.local>
2013-08-16 10:33:52 -0700

    Added blur functionality. Tabbing into and out of menu should be fully functional. All tests pass.

--- ClickBeetleJS/src/clickbeetle.js
+++ ClickBeetleJS/src/clickbeetle.js
@@ -6,21 +6,35 @@
 
     ClickBeetle.prototype = {
         $menus: [],
+        $newFocus: false,
 
         init: function(){
             var index = -1;
             this.$menus = $(document).find(".clickbeetle-menu");
 
             while(typeof this.$menus[++index] !== "undefined"){
+                this.appendTabElement(this.$menus[index]);
                 this.bindings(this.$menus[index]);
             }
         },
 
         bindings: function($menu){
-            var anchors = $($menu).find('a'),
-                children = $($menu).children();
+            var children = $($menu).children(),
+                lastChild = children[children.length - 1],
+                anchors = $($menu).find('a'),
+                firstAnchor = anchors[0];
 
+            //Focus actions
             $($menu).find('a').bind("focus", this.focusAction);
+            $(lastChild).bind("focus", this.focusAction);
+
+            //Blur actions
+            $(firstAnchor).bind("blur", this.blurAction);
+            $(lastChild).bind("blur", this.blurAction);
+        },
+
+        appendTabElement: function($menu){
+            $($menu).append("<div tabindex=\"0\"></div>");
         },
 
         focusAction: function(event){
@@ -33,9 +47,21 @@
             if(!$($target).hasClass("show")){
                 $($target).addClass("show");
             }
+
+            this.$newFocus = true;
         },
 
-        blurAction: function(){}
+        blurAction: function(event){
+            var $target = event.target;
+
+            while(!$($target).hasClass("clickbeetle-menu")){
+                $target = $($target).parent();
+            }
+
+            while($($target).hasClass("show")){
+                $($target).removeClass("show");
+            }
+        }
     }
 
     $(document).ready(function(document, $){
--- ClickBeetleJS/tests/js/clickbeetle.main.tests.js
+++ ClickBeetleJS/tests/js/clickbeetle.main.tests.js
@@ -1,11 +1,4 @@
-module("ClickBeetle", {
-        setup: function(){},
-        teardown: function(){
-            while($(".clickbeetle-menu").hasClass("show")){
-                $(".clickbeetle-menu").removeClass("show");
-            }
-        }
-    });
+module("ClickBeetle setup");
 
 test("should be available for use", function(){
     notEqual(typeof clickbeetle, "undefined");
@@ -15,15 +8,80 @@ test("should have a menu array conatining one element", function(){
     equal(clickbeetle.$menus.length, 1);
 });
 
+test("should append a div to the bottom of menu", function(){
+    var children = $($(".clickbeetle-menu")[0]).children(),
+        lastchild = children[children.length - 1];
+    equal($(lastchild).html(), "");
+});
+
+test("should place a tabindex of 0 on appended div element", function(){
+    var children = $($(".clickbeetle-menu")[0]).children(),
+        lastchild = children[children.length - 1];
+    equal($(lastchild).attr("tabindex"), "0");
+});
+
+/* Focus interactions */
+
+module("ClickBeetle focus interactions", {
+    setup: function(){},
+    teardown: function(){
+        //Remove all instances of className show
+        while($(".clickbeetle-menu").hasClass("show")){
+            $(".clickbeetle-menu").removeClass("show");
+        }
+        //Remove focus from all menu elements
+        $("#focusable").focus();
+    }
+})
+
 test("should set the menu class to show when the main link gets focus", function(){
     $($(".clickbeetle-menu").find('a')[0]).focus();
     equal($($(".clickbeetle-menu")[0]).hasClass("show"), true);
-    $($(".clickbeetle-menu").find('a')[0]).focus();
 });
 
-test("should set the menu class to show when the last link gets focus", function(){
-    var anchors = $(".clickbeetle-menu").find('a');
-    $(anchors[anchors.length - 1]).focus();
+test("should set the menu class to show when a random link gets focus", function(){
+    var anchors = $(".clickbeetle-menu").find('a'),
+        index = Math.ceil(Math.random() * (anchors.length - 1));
+    $(anchors[index]).focus();
     equal($($(".clickbeetle-menu")[0]).hasClass("show"), true);
 });
 
+test("should set the menu class to show when the appended div element is focused", function(){
+    var children = $($(".clickbeetle-menu")[0]).children(),
+        lastChild = children[children.length - 1];
+    $(lastChild).focus();
+    equal($($(".clickbeetle-menu")[0]).hasClass("show"), true);
+});
+
+/* Blur interactions */
+
+module("ClickBeetle blur interactions", {
+    setup: function(){
+        //ensure there is one instance of show set on the menu
+        if(!$($(".clickbeetle-menu")[0]).hasClass("show")){
+            $($(".clickbeetle-menu")[0]).addClass("show");
+        }
+    },
+    teardown: function(){
+        while($(".clickbeetle-menu").hasClass("show")){
+            $(".clickbeetle-menu").removeClass("show");
+        }
+        //remove focus from all menu elements
+        $("#focusable").focus();
+    }
+})
+
+test("should remove class from menu when main link loses focus", function(){
+    var mainLink = $($(".clickbeetle-menu")[0]).find('a')[0];
+    $(mainLink).focus();
+    $(mainLink).blur();
+    equal($($(".clickbeetle-menu")[0]).hasClass("show"), false);
+});
+
+test("should remove class from menu when last child loses focus", function(){
+    var children = $($(".clickbeetle-menu")[0]).children(),
+        lastChild = children[children.length - 1];
+    $(lastChild).focus();
+    $(lastChild).blur();
+    equal($($(".clickbeetle-menu")[0]).hasClass("show"), false);
+});
